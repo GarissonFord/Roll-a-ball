dde9039950d69d9d55f9ffdfb0b0a6bdab0c7209
Gary DiCiano <garydiciano4@gmail.com>
2017-05-30 21:57:53 -0400

    2. Moving the Player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	//Before rendering a frame
+	//Where most game code will go 
+	void Update()
+	{
+
+	}
+
+	//Called before performing any physics calculations
+	//Where most physics code will go 
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
